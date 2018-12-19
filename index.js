2a69a50a5234f441523eaea887210cef628ee261
Muhammed Haque <m.haque@livelinktechnology.net>
2018-12-19 16:53:42 +0000

    tests and multi level read and write

--- index.js
+++ index.js
@@ -22,33 +22,44 @@ module.exports = function(opts) {
         fs.writeFileSync(opts.filename, opts.stringify(data), opts.enc);
     }
     
-    const saveOnAccess = {
+    const saveOnAccess = path => ({
         get: (target, prop) => {
-            let val = target[prop];
+            data = opts.parse(fs.readFileSync(opts.filename, opts.enc));
+            const pathToProp = `${path}.${prop}`;
+            let val;
+            try {
+                val = eval('data.' + pathToProp);
+            } catch (e) {
+                val = target[prop]; // fallback for toJSON
+            }
             if (typeof val === 'object' && val !== null) {
-                val = new Proxy(val, saveOnAccess);
+                val = new Proxy(val, saveOnAccess(pathToProp));
             }
             return val;
         },
         set: (target, prop, val) => {
+            if (typeof val === 'object' && val !== null) {
+                val = new Proxy(val, saveOnAccess(`${path}.${prop}`));
+            }
             target[prop] = val;
+            eval(`data.${path} = target`);
             fs.writeFileSync(opts.filename, opts.stringify(data), opts.enc);
             return true;
         }
-    };
+    });
 
     return new Proxy(data, {
         get: (_, prop) => {
             data = opts.parse(fs.readFileSync(opts.filename, opts.enc));
             let val = data[prop];
             if (typeof val === 'object' && val !== null) {
-                val = new Proxy(val, saveOnAccess);
+                val = new Proxy(val, saveOnAccess(prop));
             }
             return val;
         },
         set: (_, prop, val) => {
             if (typeof val === 'object' && val !== null) {
-                val = new Proxy(val, saveOnAccess);
+                val = new Proxy(val, saveOnAccess(prop));
             }
             data[prop] = val;
             fs.writeFileSync(opts.filename, opts.stringify(data), opts.enc);
---
+++ test/index.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+
+const {
+    expect,
+} = require('chai');
+
+const persist = require('../index');
+
+function readJson(f) {
+    return JSON.parse(fs.readFileSync(f, 'utf8'));
+}
+
+describe('default', () => {
+    
+    beforeEach(() => {
+        try {
+            fs.unlinkSync('data.json');
+        } catch (e) {}
+    });
+
+    it('persists changes top level', () => {
+        const o = persist();
+
+        o.a = 1;
+
+        expect(readJson('data.json')).to.deep.equal({a:1});
+    });
+
+    it('persists changes second level', () => {
+        const o = persist();
+
+        o.a = {};
+        o.a.b = 9;
+
+        expect(readJson('data.json')).to.deep.equal({a:{b:9}});
+    });
+
+    it('persists changes second level array', () => {
+        const o = persist();
+
+        o.a = [91];
+        o.a.push(92);
+
+        expect(readJson('data.json')).to.deep.equal({a:[91, 92]});
+    });
+
+    it('persists changes detach', () => {
+        const o = persist();
+
+        o.a = [91];
+        const x = o.a;
+        x.push(93)
+
+        expect(readJson('data.json')).to.deep.equal({a:[91, 93]});
+    });
+
+    it('read changes top level', () => {
+        const o = persist();
+
+        o.a = 9;
+
+        fs.writeFileSync('data.json', '{"a":55}', 'utf8');
+
+        expect(o.a).to.equal(55);
+    });
+
+    it('read changes second level', () => {
+        const o = persist();
+
+        o.a = {b:8};
+
+        fs.writeFileSync('data.json', '{"a":{"b": 77}}', 'utf8');
+
+        expect(o.a.b).to.equal(77);
+    });
+
+    it('read changes detatched', () => {
+        const o = persist();
+
+        o.a = {b:8};
+        const x = o.a;
+
+        fs.writeFileSync('data.json', '{"a":{"b": 54}}', 'utf8');
+
+        expect(x.b).to.equal(54);
+    });
+
+});
